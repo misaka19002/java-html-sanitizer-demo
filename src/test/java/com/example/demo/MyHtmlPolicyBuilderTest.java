f2582615c11e8cb2d937e399415bba61ac63e92c
admin <wyudi@qq.com>
2019-07-05 19:55:24 +0800

    使用java-html-sanitizer防止xss攻击。过滤富文本。 过滤html标签demo

--- src/test/java/com/example/demo/MyHtmlPolicyBuilderTest.java
+++ src/test/java/com/example/demo/MyHtmlPolicyBuilderTest.java
@@ -21,11 +21,19 @@ public class MyHtmlPolicyBuilderTest {
     
     @Test
     public void testImg() {
+        /**
+         * <a></a>标签
+         * <img></img>标签
+         * 这些有属性的标签都需要特殊配置。不能仅配置allowElements
+         * 例如a标签，写了标签对儿，但是没有配置herf，也是没有办法展示的。
+         * 特殊的标签必须得配上属性拦截。
+         */
         PolicyFactory pf = new HtmlPolicyBuilder()
                 .allowElements("b", "i", "a", "img")
                 .allowAttributes("src").onElements("img")
+                .allowAttributes("herf","ff").onElements("a")
                 .toFactory();
-        String src = Joiner.on("\n").join("sdf", "<b>aaaaa</b><a></a><img/>");
+        String src = Joiner.on("\n").join("sdf", "<b>aaaaa</b><a herf=\"\">ss</a><img/><a/><a herf=\"\" ff=\"\"></a>");
         String result = pf.sanitize(src);
         System.out.println(src);
         System.out.println(result);
